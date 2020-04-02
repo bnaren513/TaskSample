6a3bf502eaf47ff308213a7294be05c458f30547
NarendraAmz <narendra.biswa@amzurtech.com>
2020-04-02 16:39:38 +0530

    Initial Commit

---
+++ SampleListTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleListTask
+//
+//  Created by Narendra Biswa on 02/04/20.
+//  Copyright © 2020 Narendra Biswa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
